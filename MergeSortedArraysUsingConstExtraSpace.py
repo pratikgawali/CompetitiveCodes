8fe45104fa8dcabb19ad998e58d2848420c6fa94
Gawali <pratik.gawali@sap.com>
2019-06-20 00:52:31 +0530

    Fixed bug in insertSorted function

--- MergeSortedArraysUsingConstExtraSpace.py
+++ MergeSortedArraysUsingConstExtraSpace.py
@@ -10,6 +10,7 @@ remaining numbers are in the second array. Extra space allowed in O(1).
 
 def merge(arr1, arr2):
     """Merges the given two sorted arrays with O(1) extra space."""
+
     i, n = 0, len(arr1)
     while i < n:
         if arr1[i] > arr2[0]:
@@ -18,8 +19,6 @@ def merge(arr1, arr2):
             insertSorted(arr2, val)
         i += 1
 
-    print(arr1, arr2)    
-
 
 def insertSorted(arr, val):
     """
@@ -27,8 +26,9 @@ def insertSorted(arr, val):
     
     It assumes the first space in the array is blank.
     """
+
     i, n = 1, len(arr)
-    while arr[i] < val and i < n:
+    while i < n and arr[i] < val:
         arr[i-1] = arr[i]
         i+=1
 
@@ -37,6 +37,7 @@ def insertSorted(arr, val):
 
 if __name__ == "__main__":
     
-    arr1 = [1,3,4,8,9,13,16]
+    arr1 = [1,3,4,8,9,13,16,19]
     arr2 = [0,2,5,10,11,14,15,18]
     merge(arr1, arr2)
+    print(arr1, arr2)
